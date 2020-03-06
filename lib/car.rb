30f1f7c50cb4f59d50756c43ce44bf05284844ac
Sasho1989 <caki.jova@gmail.com>
2020-03-06 19:21:09 +0100

    Adding new tests about defects

--- lib/car.rb
+++ lib/car.rb
@@ -5,12 +5,13 @@ module Car
     SPEED_OFFSET = 15
 
     attr_accessor :color, :current_speed
-    attr_reader :year
+    attr_reader :year, :defects
     def initialize(model, year, color)
       @model = model
       @color = color
       @year = year
       @current_speed = 0
+      @defects = []
     end
 
     def speed_up(number = SPEED_OFFSET)
@@ -19,6 +20,24 @@ module Car
 
     def slow_down(number = SPEED_OFFSET)
       @current_speed -= number
+    end
+
+    def spray_paint(arg)
+      @color = arg
+    end
+
+    def add_defects(defect)
+      @defects.push(defect)
+    end
+
+    def service_info
+      defects = @defects.join(', ')
+      "Detected defects: #{defects}"
+    end
+
+    def fix_defect(defects)
+      @defects = defects
+      @defects.delete(defects)
     end
   end
 end
--- spec/mycar_spec.rb
+++ spec/mycar_spec.rb
@@ -1,41 +1,63 @@
 RSpec.describe Car::MyCar do
+    let(:opel) { opel = Car::MyCar.new('astra', '2006', 'silver') }
+
+    let(:failure) {'water pump failure'}
+
     it "creates instance of MyCar class" do
-      volvo = Car::MyCar.new('volvo', '2019', 'yellow')
-      expect(volvo).to be_instance_of(Car::MyCar)
+      expect(opel).to be_instance_of(Car::MyCar)
     end
 
     it "has starting speed of 0" do
-        volvo = Car::MyCar.new('volvo', '2019', 'yellow')
-        expect(volvo.current_speed).to eq(0)
+      expect(opel.current_speed).to eq(0)
     end
 
     it "can speed up" do
-        opel = Car::MyCar.new('astra', '2006', 'silver')
-        opel.speed_up(100)
-        expect(opel.current_speed).to be_within(99).of(101)
+      opel.speed_up(100)
+      expect(opel.current_speed).to be_within(99).of(101)
     end
 
     it "can speed up even more" do
-      opel = Car::MyCar.new('astra', '2006', 'silver')
       opel.speed_up(150)
       expect(opel.current_speed).to be > 100
     end
 
     it "can speed up to default value" do
-      opel = Car::MyCar.new('astra', '2006', 'silver')
       opel.speed_up
       expect(opel.current_speed).to eq(Car::MyCar::SPEED_OFFSET)
     end
 
     it "can slow down" do
-      opel = Car::MyCar.new('astra', '2006', 'silver')
       opel.slow_down(50)
       expect(opel.current_speed).to eq -50
     end
 
     it "can slow down to default value" do
-      opel = Car::MyCar.new('astra', '2006', 'silver')
       opel.slow_down
       expect(opel.current_speed).to eq(-Car::MyCar::SPEED_OFFSET)
     end
-  end
+
+    it 'changes the color of the object' do
+      opel.spray_paint :red
+      expect(opel.color).to eq(:red)
+    end
+
+    it 'has no defects' do
+      expect(opel.defects).not_to include (failure)
+    end
+
+    it 'is adding defects' do
+      opel.add_defects('water pump failure')
+      expect(opel.defects).to include('water pump failure')
+    end
+
+    it 'lists all the defects' do
+      opel.add_defects('water pump failure')
+      opel.add_defects('oil leak')
+      expect(opel.service_info).to eq("Detected defects: #{failure}, oil leak")
+    end
+
+    it 'can fix a defect' do
+      opel.fix_defect('water pump failure')
+      expect(opel.defects).to eq('water pump failure')
+    end
+end
